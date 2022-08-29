4c58c8e8be028258040ceb46b03f8ab5cb94bc95
Shuvaev_Vadim <shuvaev.v.6@gmail.com>
2022-08-29 22:15:31 +0700

    Fix changes

--- Sem 4 Lab 2/BinarySearchTree.h
+++ Sem 4 Lab 2/BinarySearchTree.h
@@ -3,8 +3,6 @@
 
 typedef struct Nod BinarySearchTree;
 
-
-
 struct Nod {
     int val;
     BinarySearchTree* left;
@@ -23,25 +21,12 @@ BinarySearchTree* Init(int value) {
 BinarySearchTree* Search(BinarySearchTree* tree, int value) {
     BinarySearchTree* temp = tree;
 
-    /*while (temp->left!=NULL&&temp->right!=NULL) {
-        if (value < temp->val) temp = temp->left;
-        else temp = temp->right;
-    }*/
-    /*if (temp->left==NULL) printf("temp->left == NULL\n");
-    if (temp->right == NULL) printf("temp->right == NULL\n");*/
-
     for (; temp->val != value;) {
         if (value < temp->val) temp = temp->left;
         else temp = temp->right;
 
         if (temp == NULL) break;
     }
-
-
-
-    /*if (temp == NULL) {
-        printf("temp == NULL\n");
-    }*/
 
     return temp;
 }
@@ -59,16 +44,6 @@ BinarySearchTree* Insert(BinarySearchTree* tree, int value) {
     BinarySearchTree* temp = tree, * node = Init(value);
     BinarySearchTree* previousNod = NULL;
     int pos = 0;
-   /* for (; (temp->left != NULL && temp->right != NULL);) {
-        
-        if (value < temp->val) {
-            temp = temp->left;
-        }
-        else {
-            temp = temp->right;
-        }
-        
-    }*/
 
     for (; (temp != NULL);) {
 
@@ -84,12 +59,6 @@ BinarySearchTree* Insert(BinarySearchTree* tree, int value) {
             pos = 1;
         }
     }
-    /*if (value < temp->val) {
-        temp->left = node;
-    }
-    else {
-        temp->right = node;
-    }*/
     if (pos == 0) {
         previousNod->left = node;
     }
@@ -103,20 +72,20 @@ BinarySearchTree* Insert(BinarySearchTree* tree, int value) {
 void Remove(BinarySearchTree* tree, int value) {
     BinarySearchTree* toDelete = tree;
     static BinarySearchTree* previousNod = NULL;
-    static int pos = 0; //Если 0 - предыдущий шаг был пройден влево, 1 - вправо
+    static int pos = 0; 
     if (toDelete == NULL) return;
     else {
-        if (value < toDelete->val) { //Идет по левому поддереву
+        if (value < toDelete->val) { 
             previousNod = toDelete;
             pos = 0;
             Remove(toDelete->left, value);
         }
-        else if (value > toDelete->val) { //Идет по левому поддереву
+        else if (value > toDelete->val) { 
             previousNod = toDelete;
             pos = 1;
             Remove(toDelete->right, value);
         }
-        else { //Рассматривает текущий элемент
+        else { 
             if (toDelete->left == NULL && toDelete->right == NULL) { 
                 if (pos == 0) previousNod->left = NULL;
                 else previousNod->right = NULL;
@@ -126,18 +95,12 @@ void Remove(BinarySearchTree* tree, int value) {
                 
             else if ((toDelete->left == NULL) || (toDelete->right == NULL)) {
                 if (toDelete->left == NULL) {
-                    /*toDelete->val = toDelete->right->val;
-                    toDelete->left = toDelete->right->left;
-                    toDelete->right = toDelete->right->right;*/
                     if (pos == 0) previousNod->left = toDelete->right;
                     else previousNod->right = toDelete->right;
 
                     free(toDelete);
                 }
                 else {
-                    /*toDelete->val = toDelete->left->val;
-                    toDelete->right = toDelete->left->right;
-                    toDelete->left = toDelete->left->left;*/
                     if (pos == 0) previousNod->left = toDelete->left;
                     else previousNod->right = toDelete->left;
 
@@ -145,39 +108,6 @@ void Remove(BinarySearchTree* tree, int value) {
                 }
             }
             else {
-                /*if (toDelete->right->left == NULL) {
-                    if (previousNod != NULL) 
-                    {
-                        if (pos == 0) {
-                            previousNod->left = toDelete->right;
-                        }
-                        else {
-                            previousNod->right = toDelete->right;
-                        }
-                        free(toDelete);
-
-                    }
-                    else {
-                        BinarySearchTree* temp = toDelete->right;
-                        previousNod = toDelete;
-
-                        for (; temp->left != NULL;  temp = temp->left) previousNod = temp;
-
-                        previousNod->left = NULL;
-
-                        temp->left = toDelete->left;
-                        temp->right = toDelete->right;
-
-                        free(toDelete);
-                    }
-                }*/
-                /*if (previousNod != NULL) {
-
-                }
-                else {
-                    toDelete->right->left = toDelete->left;
-                    free(toDelete);
-                }*/
                 BinarySearchTree* min = Min(toDelete->right);
                 toDelete->val = min->val;
                 free(min);
